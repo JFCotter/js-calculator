cd6d43f690cecef69e92c3e9dd35dd2945527b87
JFCotter <james.cotter@ageas.co.uk>
2023-12-07 15:10:26 +0000

    first attempt

---
+++ scripts/calculator.js
@@ -0,0 +1,21 @@
+// CALCULATOR
+
+"use strict";
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "ERROR";
+  }
+}
