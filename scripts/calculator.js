5575df989ac68d06d5b89e1b9d90369b2d44853f
JFCotter <james.cotter@ageas.co.uk>
2023-12-13 11:19:17 +0000

    Ben's Sage Advice

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -3,34 +3,68 @@
 "use strict";
 
 //CONSTANTS
-const calculator = {
-  displayValue: "0",
-};
+// const calculator = {
+//   displayValue: "0",
+// };
 
-const previousOperand = document.querySelector("[data-previousOperand]");
-const currentOperand = document.querySelector("[data-currentOperand]");
-const numberButtons = document.querySelectorAll("[data-number]");
-const operatorButtons = document.querySelectorAll("[data-operator]");
-const deleteButton = document.querySelector("[data-delete]");
-const clearButton = document.querySelector("[data-clear]");
-const equalsButton = document.querySelector("[data-equals]");
+// const previousOperand = document.querySelector("[data-previousOperand]");
+// const currentOperand = document.querySelector("[data-currentOperand]");
+// const numberButtons = document.querySelectorAll("[data-number]");
+// const operatorButtons = document.querySelectorAll("[data-operator]");
+// const deleteButton = document.querySelector("[data-delete]");
+// const clearButton = document.querySelector("[data-clear]");
+// const equalsButton = document.querySelector("[data-equals]");
 
 // FUNCTIONS
 
-function updateDisplay() {
-  currentOperand.innerText = calculator.displayValue;
-}
+// function updateDisplay() {
+//   currentOperand.innerText = calculator.displayValue;
+// }
 
-updateDisplay();
+// updateDisplay();
+
+
+
+
 
 /* Ben started ruining it hereinafter... */
 
-numberButtons.forEach(
-  numberButton => numberButton.addEventListener(
+const getValidOperandChar_OrNothing = (operandChar) => 
+  "0123456789.".includes(operandChar) ? operandChar : ""
+;
+
+const primaryOperand = {
+  element   : document.querySelector("[data-primary-operand]"),
+  addDigit  : (newDigit) => primaryOperand.element.innerText += getValidOperandChar_OrNothing(newDigit),
+  clear     : () => primaryOperand.element.innerText = "",
+  get       : () => parseFloat(primaryOperand.element.innerText)
+};
+
+const secondryOperand = {
+  element : document.querySelector("[data-secondary-operand]"),
+  addDigit  : (newDigit) => secondryOperand.element.innerText += getValidOperandChar_OrNothing(newDigit),
+  clear     : () => secondryOperand.element.innerText = "",
+  get       : () => parseFloat(secondryOperand.element.innerText)
+};
+
+/* Digits */
+document.querySelectorAll("[data-number]").forEach(
+  (numberButton) => numberButton.addEventListener(
       "click",
-      () => console.log("Number Button CLicked!")
-    );
+      () => primaryOperand.addDigit(numberButton.innerText)
+  )
 );
 
-function (p1, p2) { return p1; }
-(p1, p2) => p1
+/* Operators */
+document.querySelectorAll("[data-number]").forEach(
+  (numberButton) => numberButton.addEventListener(
+      "click",
+      () => primaryOperand.addDigit(numberButton.innerText)
+  )
+);
+
+
+/*
+  function (p1, p2) { return p1; }
+  (p1, p2) => p1
+*/
