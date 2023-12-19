93068daf31f7178544594d7233a52fdde5d9eba6
JFCotter <james.cotter@ageas.co.uk>
2023-12-19 16:18:46 +0000

    wow it actually does something

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,59 +1,86 @@
 // CALCULATOR OBJECT
 const calculator = {
-  currentOperand: "0",
-  previousOperand: null,
-  waitingForSecondOperand: false,
+  currentOperand: "",
+  previousOperand: "",
   operator: null,
-};
+}
 // REFRESH DISPLAY
 function updateDisplay() {
-  const bottomDisplay = document.querySelector("[data-current-operand]");
-  bottomDisplay.innerText = calculator.currentOperand;
-  const topDisplay = document.querySelector("[data-previous-operand]")
-  topDisplay.innerText = calculator.previousOperand
+  document.querySelector("[data-current-operand]").innerText = calculator.currentOperand;
+  document.querySelector("[data-previous-operand]").innerText = calculator.previousOperand;
 }
-
+//DETECT WHEN CLEAR BUTTON IS PUSHED
+document.querySelector("[data-clear]").addEventListener(
+  "click", clearCalculator)
+//CLEAR CALCULATOR
+function clearCalculator() {
+  calculator.currentOperand = "";
+  calculator.previousOperand = "";
+  calculator.operator = null;
+  updateDisplay()
+}
+//DETECT WHEN CLEAR BUTTON IS PUSHED
+document.querySelector("[data-delete]").addEventListener(
+  "click", deleteCurrentOperand)
+//DELETE LAST INTERGER OF CURRENT OPERAND
+function deleteCurrentOperand() {
+  calculator.currentOperand = calculator.currentOperand.slice(0, -1);
+  updateDisplay()
+}
 //DETECT WHEN NUMBER BUTTON IS PUSHED
 document.querySelectorAll("[data-number]").forEach(
   (numberButton) => numberButton.addEventListener(
-    "click", () => calculator.currentOperand.addDigit(numberButton.innerText)
-)
-)
+    "click", addDigit))
 
 //APPEND NUMBER TO CURRENT OPERAND
-function addDigit() {
+function addDigit(e) {
+  const digit = e.target.getAttribute("data-number")
   //Maximum of one decimal point
   if (digit === "." && calculator.currentOperand.includes(".")) return
-  //
-    calculator.currentOperand = currentOperand === '0' ? digit : currentOperand + digit;
-  }
+  //Append digit pressed
+  calculator.currentOperand += digit
+  updateDisplay()
+
+  //  calculator.currentOperand = currentOperand === '0' ? digit : currentOperand += digit;
+}
 
 
 //DETECT WHEN OPERATOR IS SELECTED
-
-//MOVE CURRENT OPERAND TO PREVIOUS OPERAND, SET "WAITING FOR SECOND OPERAND TO TRUE"
-
-
+document.querySelectorAll("[data-operator]").forEach(
+  (operatorButton) => operatorButton.addEventListener(
+    "click", chooseOperator
+  )
+)
+function chooseOperator(e) {
+  calculator.operator = e.target.getAttribute("data-operator")
+  calculator.previousOperand = calculator.currentOperand + e.target.getAttribute("data-operator")
+  calculator.currentOperand = ""
+  updateDisplay()
+}
+//DETECT WHEN ANSWER BUTTON IS SELECTED
+document.querySelector("[data-equals]").addEventListener(
+  "click", calculate)
 //CALCULATE
-function calculate(previousOperand, currentOperand, operator) {
-  if (operator === "+") {
-    return previousOperand + currentOperand;
-  } else if (operator === "-") {
-    return previousOperand - currentOperand;
-  } else if (operator === "&times") {
-    return previousOperand * currentOperand;
-  } else if (operator === "&divide") {
-    return previousOperand / currentOperand;
+function calculate(answer) {
+  const previous = parseFloat(calculator.previousOperand)
+  const current = parseFloat(calculator.currentOperand)
+  switch (calculator.operator) {
+    case "+":
+      answer = previous + current
+      break
+    case "-":
+      answer = previous - current
+      break
+    case "*":
+      answer = previous * current
+      break
+    case "/":
+      answer = previous / current
+      break
+    default: return
   }
-
-  return currentOperand;
+  calculator.currentOperand = answer
+  calculator.previousOperand = ""
+  calculator.operator = null
+  updateDisplay()
 }
-
-//CLEAR CALCULATOR
-function clearCalculator() {
-  calculator.currentOperand = "0";
-  calculator.previousOperand = null;
-  calculator.waitingForSecondOperand = false;
-  calculator.operator = null;
-}
-//DELETE
